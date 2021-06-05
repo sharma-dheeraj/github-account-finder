6864616353e6027db1493d397435d5547742bc57
Dheeraj Sharma <dhiru3599@gmail.com>
2021-06-06 00:33:50 +0530

    refactor: pass User as prop to UserItem instead of state

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,23 +1,17 @@
 import React, { Component } from "react";
 
 class UserItem extends Component {
-  state = {
-    id: "1",
-    login: "mojombo",
-    avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-    html_url: "https://github.com/mojombo",
-  };
   render() {
     return (
       <div className="card text-center">
         <img
-          src={this.state.avatar_url}
+          src={this.props.user.avatar_url}
           className="round-img"
           style={{ width: "100px" }}
           alt=""
         />
-        <h4>{this.state.login}</h4>
-        <a href={this.state.html_url} className="btn btn-dark">
+        <h4>{this.props.user.login}</h4>
+        <a href={this.props.user.html_url} className="btn btn-dark">
           Profile
         </a>
       </div>
