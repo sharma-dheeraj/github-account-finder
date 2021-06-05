38f943e1390c51e2b398c2495674051fc3faa712
Dheeraj Sharma <dhiru3599@gmail.com>
2021-06-06 03:33:22 +0530

    feat: add user data with github api

--- src/App.js
+++ src/App.js
@@ -4,12 +4,23 @@ import Users from "./components/users/Users";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  componentDidMount() {
+    this.setState({ loading: true });
+    fetch("https://api.github.com/users")
+      .then((res) => res.json())
+      .then((data) => this.setState({ loading: false, users: data }));
+  }
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
