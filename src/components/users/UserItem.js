7293df10d912e23e9fe5102b344870095827e037
Dheeraj Sharma <dhiru3599@gmail.com>
2021-06-16 13:44:12 +0530

    refactor: use {link} instead of anchor to link user details page

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const UserItem = ({ user: { avatar_url, login, html_url } }) => {
   return (
@@ -11,9 +12,9 @@ const UserItem = ({ user: { avatar_url, login, html_url } }) => {
         alt=""
       />
       <h4>{login}</h4>
-      <a href={html_url} className="btn btn-dark">
+      <Link to={`/user/${login}`} className="btn btn-dark">
         Profile
-      </a>
+      </Link>
     </div>
   );
 };
