5921e06002190dec834ebda245dad4e18491be66
Dheeraj Sharma <dhiru3599@gmail.com>
2021-06-17 22:30:09 +0530

    refactor: change class component to functional and using useState

--- src/components/users/SearchBar.js
+++ src/components/users/SearchBar.js
@@ -1,52 +1,47 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-class SearchBar extends Component {
-  state = {
-    searchbar: "",
+const SearchBar = ({ setAlert, searchUsers, showClear, clearUsers }) => {
+  const [searchbar, setSearchbar] = useState("");
+
+  const onChange = (event) => {
+    setSearchbar(event.target.value);
   };
 
-  onChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
-  onSubmit = (event) => {
+  const onSubmit = (event) => {
     event.preventDefault();
-    if (this.state.searchbar === "") {
-      this.props.setAlert("Please enter something", "light");
+    if (searchbar === "") {
+      setAlert("Please enter something", "light");
     } else {
-      this.props.searchUsers(this.state.searchbar);
-      this.setState({ searchbar: "" });
+      searchUsers(searchbar);
+      setSearchbar("");
     }
   };
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.onSubmit} className="form">
-          <input
-            type="text"
-            name="searchbar"
-            placeholder="Search..."
-            value={this.state.searchbar}
-            onChange={this.onChange}
-          />
-          <input
-            type="submit"
-            value="Search"
-            className="btn btn-dark btn-block"
-          />
-        </form>
-        {this.props.showClear && (
-          <button
-            className="btn btn-light btn-block"
-            onClick={this.props.clearUsers}
-          >
-            Clear
-          </button>
-        )}
-      </div>
-    );
-  }
-}
+
+  return (
+    <div>
+      <form onSubmit={onSubmit} className="form">
+        <input
+          type="text"
+          name="searchbar"
+          placeholder="Search..."
+          value={searchbar}
+          onChange={onChange}
+        />
+        <input
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
+        />
+      </form>
+      {showClear && (
+        <button className="btn btn-light btn-block" onClick={clearUsers}>
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
 
 SearchBar.propTypes = {
   searchUsers: PropTypes.func.isRequired,
