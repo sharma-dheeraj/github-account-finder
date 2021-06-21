9a05902cd583c28cec27f0e1069ed18f82419531
Dheeraj Sharma <dhiru3599@gmail.com>
2021-06-21 23:49:23 +0530

    feat: fetch searchUsers state via GithubState instead of state from app.js

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -18,6 +18,29 @@ const GithubState = (props) => {
   };
   const [state, dispatch] = useReducer(GithubReducer, initialState);
 
+  // SEARCH FOR ARRAY OF USERS WITH MATCHING RESULTS
+  const searchUsers = async (search) => {
+    setLoading(true);
+    fetch(
+      `https://api.github.com/search/users?q=${search}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({ type: SEARCH_USERS, payload: data.items });
+      });
+  };
+
+  // GET USER
+
+  // GET REPOS
+
+  // CLEAR USERS
+
+  // SET LOADING
+  const setLoading = () => {
+    dispatch({ type: SET_LOADING });
+  };
+
   return (
     <GithubContext.Provider
       value={{
@@ -25,6 +48,7 @@ const GithubState = (props) => {
         user: state.user,
         repos: state.repos,
         loading: state.loading,
+        searchUsers,
       }}
     >
       {props.children}
