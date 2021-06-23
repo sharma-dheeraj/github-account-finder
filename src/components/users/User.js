4e3d664f7d835ebc44214e06112fe42cd20a411e
Dheeraj Sharma <dhiru3599@gmail.com>
2021-06-24 00:38:28 +0530

    feat: use context api to get single user information

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,10 +1,14 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import Spinner from "../layout/Spinner";
 import PropTypes from "prop-types";
 import Repos from "../repos/Repos";
+import GithubContext from "../../context/github/githubContext";
 
-const User = ({ loading, repos, user, getUser, getUserRepos, match }) => {
+const User = ({ repos, getUserRepos, match }) => {
+  const githubContext = useContext(GithubContext);
+  const { getUser, loading, user } = githubContext;
+
   useEffect(() => {
     getUser(match.params.login);
     getUserRepos(match.params.login);
@@ -101,9 +105,7 @@ const User = ({ loading, repos, user, getUser, getUserRepos, match }) => {
 
 User.propTypes = {
   loading: PropTypes.bool,
-  user: PropTypes.object.isRequired,
   repos: PropTypes.array.isRequired,
-  getUser: PropTypes.func.isRequired,
   getUserRepos: PropTypes.func.isRequired,
 };
 
