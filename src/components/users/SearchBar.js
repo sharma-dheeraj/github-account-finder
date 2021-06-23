7fb3f8e4043a96f602ccd1550434054e72cdd881
Dheeraj Sharma <dhiru3599@gmail.com>
2021-06-23 22:33:19 +0530

    refactor: update users array with github state

--- src/components/users/SearchBar.js
+++ src/components/users/SearchBar.js
@@ -2,7 +2,7 @@ import React, { useState, useContext } from "react";
 import PropTypes from "prop-types";
 import GithubContext from "../../context/github/githubContext";
 
-const SearchBar = ({ setAlert, showClear, clearUsers }) => {
+const SearchBar = ({ setAlert }) => {
   const githubContext = useContext(GithubContext);
   const [searchbar, setSearchbar] = useState("");
 
@@ -36,8 +36,11 @@ const SearchBar = ({ setAlert, showClear, clearUsers }) => {
           className="btn btn-dark btn-block"
         />
       </form>
-      {showClear && (
-        <button className="btn btn-light btn-block" onClick={clearUsers}>
+      {githubContext.users.length > 0 && (
+        <button
+          className="btn btn-light btn-block"
+          onClick={githubContext.clearUsers}
+        >
           Clear
         </button>
       )}
@@ -46,8 +49,6 @@ const SearchBar = ({ setAlert, showClear, clearUsers }) => {
 };
 
 SearchBar.propTypes = {
-  clearUsers: PropTypes.func.isRequired,
-  showClear: PropTypes.bool.isRequired,
   setAlert: PropTypes.func.isRequired,
 };
 
