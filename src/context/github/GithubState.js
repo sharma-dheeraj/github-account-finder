b9cf9aa83503ecfee8e3998bdaae73773930b3e9
Dheeraj Sharma <dhiru3599@gmail.com>
2021-06-24 14:54:53 +0530

    refactor: add different environment variable values for production in server and local variables

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -9,6 +9,17 @@ import {
   GET_REPOS,
 } from "../types";
 
+let githubClientId;
+let githubClientSecret;
+
+if (process.env.NODE_ENV !== "production") {
+  githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+  githubClientId = process.env.GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
+
 const GithubState = (props) => {
   const initialState = {
     users: [],
@@ -22,7 +33,7 @@ const GithubState = (props) => {
   const searchUsers = async (search) => {
     setLoading(true);
     fetch(
-      `https://api.github.com/search/users?q=${search}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${search}&client_id=${githubClientId}&client_secret=${githubClientSecret}`
     )
       .then((res) => res.json())
       .then((data) => {
@@ -34,7 +45,7 @@ const GithubState = (props) => {
   const getUser = async (username) => {
     setLoading();
     fetch(
-      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/users/${username}?client_id=${githubClientId}&client_secret=${githubClientSecret}`
     )
       .then((res) => res.json())
       .then((data) => {
@@ -46,7 +57,7 @@ const GithubState = (props) => {
   const getUserRepos = async (username) => {
     setLoading();
     fetch(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${githubClientId}&client_secret=${githubClientSecret}`
     )
       .then((res) => res.json())
       .then((data) => {
