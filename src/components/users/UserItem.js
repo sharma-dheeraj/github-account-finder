890eb4b0712eeaf3bcbec8552e3a0d3d23af8d42
Dheeraj Sharma <dhiru3599@gmail.com>
2021-06-25 02:02:25 +0530

    refactor: remove non required props

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-const UserItem = ({ user: { avatar_url, login, html_url } }) => {
+const UserItem = ({ user: { avatar_url, login } }) => {
   return (
     <div className="card text-center">
       <img
